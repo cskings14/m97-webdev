81b9e9c190dcba4fed0f292e956045c641df085c
cskings14 <cskings14@gmail.com>
2022-12-02 02:35:39 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import * as bootstrap from 'bootstrap';
+
+let btn = document.getElementById('cta-btn');
+let overlay = document.getElementById('overlay');
+
+btn.addEventListener('click', () => {
+    
+    overlay.style.display = 'grid';
+    overlay.classList.add('animate-overlay')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
